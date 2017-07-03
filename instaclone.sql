a7c438f1bd50e34f8a0ff3d7a959c34722ceb2b8
Jon Huh <oneandwholly@gmail.com>
2017-07-03 06:58:33 +0000

    create database and add users table

---
+++ instaclone.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE instaclone;
+USE instaclone;
+
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(255) UNIQUE NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW()
+);
