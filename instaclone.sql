112ff0ddd41326c395c3443f85433fe7ccbd0942
Jon Huh <oneandwholly@gmail.com>
2017-07-03 08:03:23 +0000

    add follows table

--- instaclone.sql
+++ instaclone.sql
@@ -33,3 +33,12 @@ CREATE TABLE likes (
     FOREIGN KEY(photo_id) REFERENCES photos.id,
     PRIMARY KEY(user_id, photo_id)
 );
+
+CREATE TABLE follows (
+    follower_id INT NOT NULL,
+    followee_id INT NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW(),
+    FOREIGN KEY(follower_id) REFERENCES users.id,
+    FOREIGN KEY(followee_id) REFERENCES users.id,
+    PRIMARY KEY(follower_id, followee_id)
+)
